70d06619ed1e7a762ea86f4efa0b3a07b57dfef2
Nicole <nzolnier@gmail.com>
2020-10-03 17:51:28 -0300

    fiz o nossoJogo em consts e fiz os outros desafios naquela base

--- semana3/blackjack/desafio.js
+++ semana3/blackjack/desafio.js
@@ -10,7 +10,9 @@
  * 
  * 
  */
+
 // MINHA CABEÇA TA FRITANDO E MEU NOTEBOOK TBM 
+// SERIO EU NEM SEI MAIS O QUE TA ACONTECENDO AQUI
 
 console.log("Bem vindo ao jogo de Blackjack!")
 
@@ -26,29 +28,29 @@ let pontosPc = []
 let printCartasPc = ""
 let printPontosPc = 0
 
-if(inicioJogo) {
+if (inicioJogo) {
    // sorteio de cartas pro usuario sem ser duas A's
-   for (let i=0; i < 2;) {
+   for (let i = 0; i < 2;) {
       let carta = comprarCarta()
       cartasUser.push(carta.texto)
       pontosUser.push(carta.valor)
-      if ((cartasUser[0] === 'A') && (cartasUser[1] === 'A')) {
+      if ((pontosUser[0] === 11) && (pontosUser[1] === 11)) {
          i = i
       } else {
          i++
       }
    }
-   
+
    for (let carta of cartasUser) {
       printCartasUser += (carta + " ")
    }
 
    // sorteio de cartas pro pc sem ser duas A's
-   for (let i=0; i < 2;) {
+   for (let i = 0; i < 2;) {
       let carta = comprarCarta()
       cartasPc.push(carta.texto)
       pontosPc.push(carta.valor)
-      if ((cartasPc[0] === 'A') && (cartasPc[1] === 'A')) {
+      if ((pontosPc[0] === 11) && (pontosPc[1] === 11)) {
          i = i
       } else {
          i++
@@ -68,18 +70,69 @@ if(inicioJogo) {
       printPontosPc += valor
    }
 
-   //  console de cartas e pontuaçao & resultado final por enquanto 
-   console.log(`Usuário - ${printCartasUser} - pontuação ${printPontosUser}`)
-   console.log(`Computador - ${printCartasPc} - pontuação ${printPontosPc}`)
-
-   if (pontosUser > pontosPc) {
-      console.log('O usuário venceu!')
-   } else if (pontosUser < pontosPc) {
-      console.log('O computador venceu!') 
-   } else if (pontosUser = pontosPc) {
-      console.log('Empate!')
+   // depois de sortear as cartas, vai analisar se um deles ganhou, se nao continua o caos
+   if (printPontosUser === 21) {
+      console.log(`Suas cartas são ${printCartasUser} e as do computador ${printCartasPc}`)
+      console.log("A pontuação do usuário é", printPontosUser, "e a do computador é", printPontosPc)
+      console.log("O usuário ganhou!")
    }
 
+   else if (printPontosPc === 21) {
+      console.log(`Suas cartas são ${printCartasUser} e as do computador ${printCartasPc}`)
+      console.log("A pontuação do usuário é", printPontosUser, "e a do computador é", printPontosPc)
+      console.log("O computador ganhou!")
+   }
+
+   else {
+      let continuarJogo = true
+      while (continuarJogo) {
+
+         let comprarMais = confirm(`Suas cartas são ${printCartasUser}. A carta revelada do computador é ${printCartasPc[0]}${printCartasPc[1]}
+         Deseja comprar mais uma carta?`)
+
+         if (comprarMais) {
+            let carta = comprarCarta()
+            cartasUser.push(carta.texto)
+            pontosUser.push(carta.valor)
+            printCartasUser += (carta.texto + " ")
+            printPontosUser += carta.valor
+         }
+         else {
+            comprarMais = false
+            continuarJogo = false
+            let vezDoPc = true
+
+            while (vezDoPc) {
+               if (printPontosPc < printPontosUser) {
+                  let carta = comprarCarta()
+                  cartasPc.push(carta.texto)
+                  pontosPc.push(carta.valor)
+                  printCartasPc += (carta.texto + " ")
+                  printPontosPc += carta.valor
+               }
+               else {
+                  vezDoPc = false
+                  console.log(`Suas cartas são ${printCartasUser} e as do computador ${printCartasPc}`)
+                  console.log("A pontuação do usuário é", printPontosUser, "e a do computador é", printPontosPc)
+
+                  if ((printPontosPc === printPontosUser) || (printPontosUser > 21 && printPontosPc > 21)) {
+                     console.log("Empate! Os dois perderam")
+                  }
+                  else if ((printPontosUser <= 21) || (printPontosPc > 21) || (printPontosUser > printPontosPc)) {
+                     console.log("O usuário ganhou!")
+                  }
+                  else if ((printPontosPc <= 21) || (printPontosUser > 21) || (printPontosPc > printPontosUser)) {
+                     console.log("O computador ganhou!")
+                  }
+
+
+               }
+            }
+
+         }
+      }
+
+   }
 
 }
 else {
--- semana3/blackjack/nossoJogo.js
+++ semana3/blackjack/nossoJogo.js
@@ -1,21 +1,55 @@
 /**
  * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
- * 
+ *
 
- * 
+ *
     const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
-    
+
     console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
     console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
- * 
- * 
- * 
+ *
+ *
+ *
  */
 
 
+// const para cada carta
+// console.log("Bem vindo ao jogo de Blackjack!")
+// const inicioJogo = confirm('Quer iniciar uma nova rodada?')
+
+// if(inicioJogo){
+
+//    const carta1User = comprarCarta()
+//    const carta2User = comprarCarta()
+//    const carta1Pc = comprarCarta()
+//    const carta2Pc = comprarCarta()
+
+//    let pontosUser = carta1User.valor + carta2User.valor
+//    let pontosPc = carta1Pc.valor + carta2Pc.valor
+
+//    console.log(`Usuário - ${carta1User.texto} ${carta2User.texto} - pontuação ${pontosUser}`)
+//    console.log(`Computador - ${carta1Pc.texto} ${carta2Pc.texto} - pontuação ${pontosPc}`)
+
+//    if (pontosUser > pontosPc) {
+//    console.log('O usuário venceu!')
+//    } else if (pontosUser < pontosPc) {
+//    console.log('O computador venceu!')    
+//    } else if (pontosUser = pontosPc) {
+//    console.log('Empate!')
+//    }
+// }
+
+
+// else {
+//    console.log('O jogo acabou')
+// }
+
+
+// array de cartas
 console.log("Bem vindo ao jogo de Blackjack!")
 const inicioJogo = confirm('Quer iniciar uma nova rodada?')
 
+if(inicioJogo){
    //array de cartas para cada um 
    const cartasUser = [comprarCarta(), comprarCarta()]
    const cartasPc = [comprarCarta(), comprarCarta()]
@@ -38,7 +72,7 @@ const inicioJogo = confirm('Quer iniciar uma nova rodada?')
       printCartasPc += ` ${carta.texto}`
    }
 
-   
+
    console.log(`Usuário - ${printCartasUser} - pontuação ${pontosUser}`)
    console.log(`Computador - ${printCartasPc} - pontuação ${pontosPc}`)
 
@@ -50,7 +84,10 @@ const inicioJogo = confirm('Quer iniciar uma nova rodada?')
       console.log('Empate!')
    }
 
+}
+
 else {
    console.log('O jogo acabou')
 }
+
 
