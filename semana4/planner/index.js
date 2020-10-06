70b5b53f0280d2cb41f7d5edee7bade0447d75cb
Nicole <nzolnier@gmail.com>
2020-10-06 19:18:10 -0300

    desafios completados e um pouco de estilização no css

--- semana4/planner/index.js
+++ semana4/planner/index.js
@@ -1,12 +1,14 @@
 function criarTarefa() {
     const inputTarefa = document.getElementById("tarefa")
     const inputDiaSemana = document.getElementById("dias-semana").value
-    const escolherSemana = document.getElementById(inputDiaSemana)
+    const horarioTarefa = document.getElementById("horario").value
+    const escolherDia = document.getElementById(inputDiaSemana)
+
     if (inputTarefa.value === ""){
         alert("Epa!Escreva uma tarefa!")
     }
     else {
-        escolherSemana.innerHTML += `<li onclick ="tarefaRiscada(this)">${inputTarefa.value}</li>`
+        escolherDia.innerHTML += `<li class="item" onclick ="tarefaRiscada(this)"> ${horarioTarefa} - ${inputTarefa.value}</li>`
         inputTarefa.value = ""
     }
     
@@ -20,11 +22,16 @@ function tarefaRiscada (id) {
 
 function apagarTudo(){
     const certeza = confirm("Você tem certeza que quer apagar tudo?")
+    const apagarLista = document.getElementsByClassName("lista")
+
     if (certeza){
-        
+        for (let i = 0; i < apagarLista.length; i++){
+            apagarLista[i].innerHTML = " "
+        }
     }
     else{
-
+        return
     }
 }
+
 
