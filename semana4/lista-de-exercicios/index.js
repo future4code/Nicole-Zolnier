7325668c190f66c216a7437a02b0754269983818
Nicole <nzolnier@gmail.com>
2020-10-09 11:20:27 -0300

    primeiro ex de interpretacao de codigo feito

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,20 @@
+// -------- ex de interpretação de código --------
+
+// ----- 1
+/*
+a função converte o dinheiro de dolar pra real
+
+ela espera receber o valor em dolar (esse é o parametro)
+dai ela manda um prompt pro usuario e pede pra ele informar o valor da cotação do dolar, ele transforma o que ele digitou em numero e guarda na variavel cotacao
+dai ele faz um return com o R$ e a multiplicação do valor em dolar com a cotacao
+
+pra essa função funcionar, ela foi jogada em outra const (meuDinheiro) e tá recebendo o valor 100
+dai dá um console.log nessa const de meuDinheiro
+
+entãoooo
+se o usuario digitar 5.54 como cotação do dolar naquele prompt, o que vai aparecer no console é.....
+R$ 554
+*/
+
+
+
