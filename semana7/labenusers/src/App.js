6266edb48f5bd8835d4afd52f1d42b75d881c838
Nicole <nzolnier@gmail.com>
2020-10-28 13:11:16 -0300

    mudando as coisas pro jeito do severo

--- semana7/labenusers/src/App.js
+++ semana7/labenusers/src/App.js
@@ -2,108 +2,26 @@
 import React from "react";
 import CreateUser from './components/CreateUser'
 import UserList from './components/UserList'
-import axios from 'axios';
+
 
 class App extends React.Component {
   state = {
     seeList: false,
-    users: [],
-    nameValue: "",
-    emailValue: ""
   }
 
   // funçoes pra mudar pagina
-  checkoutUserList = () => {
-    this.setState({ seeList: true })
+  checkoutOtherPage = () => {
+    this.setState({ seeList: !this.state.seeList })
   }
 
-  checkoutCreateUser = () => {
-    this.setState({ seeList: false })
-  }
-
-  // funçoes de input controlado
-  onChangeInputName = (event) => {
-    this.setState({ nameValue: event.target.value })
-  }
-
-  onChangeInputEmail = (event) => {
-    this.setState({ emailValue: event.target.value })
-  }
-
-
-  // --------- funçoes de dados
-  // função de pegar os usuarios
-  getAllUsers = () => {
-    axios.get("https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users", {
-      headers: {
-        Authorization: "nicole-zolnier-dumont"
-      }
-    }).then((answer) => {
-      this.setState({ users: answer.data })
-    }).catch((err) => {
-      console.log(err.message)
-    })
-  }
-
-
-  // função de criar usuarios
-  createUser = () => {
-    const body = {
-      name: this.state.nameValue,
-      email: this.state.emailValue
-    }
-
-    axios.post("https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users", body, {
-      headers: {
-        Authorization: "nicole-zolnier-dumont"
-      }
-    }).then((answer) => {
-      this.setState({ nameValue: "" })
-      this.setState({ emailValue: "" })
-      window.alert("Usuário cadastrado com sucesso!")
-
-      this.getAllUsers()
-    }).catch((err) => {
-      console.log(err.message)
-      window.alert("Ih deu erro! Você colocou um @ e um .com? Não pode repetir nomes também!!")
-    })
-  }
-
-  // função de deletar usuario
-  deleteUser = (userId) => {
-    const beSure = window.confirm("Tem certeza que quer deletar o usuário?")
-    if (beSure) {
-      axios.delete(`https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users/${userId}`, {
-        headers: {
-          Authorization: "nicole-zolnier-dumont"
-        },
-        params: {
-          id: userId
-        }
-      }).then((answer) => {
-        window.alert("Usuário deletado!")
-        this.getAllUsers()
-      }).catch((err) => {
-        console.log(err.message)
-      })
-    }
-  }
 
   render() {
     // funçao pra renderizar a lista correta
     const renderScreen = () => {
       if (this.state.seeList) {
-        return <UserList goBack={this.checkoutCreateUser}
-          users={this.state.users}
-          getAllUsers={this.getAllUsers}
-          deleteUser={this.deleteUser} />;
+        return <UserList goBack={this.checkoutOtherPage} />;
       } else {
-        return <CreateUser nextPage={this.checkoutUserList}
-          onChangeName={this.onChangeInputName}
-          onChangeEmail={this.onChangeInputEmail}
-          nameValue={this.state.nameValue}
-          emailValue={this.state.emailValue}
-          createUser={this.createUser} />;
+        return <CreateUser nextPage={this.checkoutOtherPage} />;
       }
     }
     return (
--- semana7/labenusers/src/components/CreateUser.js
+++ semana7/labenusers/src/components/CreateUser.js
@@ -1,6 +1,8 @@
 import styled from 'styled-components'
 import React from "react";
+import axios from 'axios';
 
+// --------- css
 const MotherDiv = styled.div`
     display: flex;
     justify-content: center;
@@ -21,7 +23,47 @@ const Goforward = styled.button`
     margin: 2em;
 `
 
+// --------- resumindo as coisas pro axios
+const baseUrl = "https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users"
+
+const axiosConfig = {
+    headers: {
+        Authorization: "nicole-zolnier-dumont"
+    }
+}
+
 class CreateUser extends React.Component {
+    state = {
+        nameValue: "",
+        emailValue: ""
+    }
+
+    // funçoes de input controlado
+    onChangeInputName = (event) => {
+        this.setState({ nameValue: event.target.value })
+    }
+
+    onChangeInputEmail = (event) => {
+        this.setState({ emailValue: event.target.value })
+    }
+
+    // função de criar usuarios
+    createUser = () => {
+        const body = {
+            name: this.state.nameValue,
+            email: this.state.emailValue
+        }
+
+        axios.post(baseUrl, body, axiosConfig).then(() => {
+            this.setState({ nameValue: "", emailValue: "" })
+            window.alert("Usuário cadastrado com sucesso!")
+
+        }).catch((err) => {
+            console.log(err.message)
+            window.alert("Ih deu erro! Você colocou um @ e um .com? Não pode repetir nomes também!!")
+        })
+    }
+
     render() {
         return (
             <div>
@@ -30,13 +72,13 @@ class CreateUser extends React.Component {
                 <MotherDiv>
                     <NiceInput
                         placeholder="nome"
-                        value={this.props.nameValue}
-                        onChange={this.props.onChangeName} />
+                        value={this.state.nameValue}
+                        onChange={this.onChangeInputName} />
                     <NiceInput type="text"
                         placeholder="email"
-                        value={this.props.emailValue}
-                        onChange={this.props.onChangeEmail} />
-                    <button onClick={this.props.createUser}>Enviar</button>
+                        value={this.state.emailValue}
+                        onChange={this.onChangeInputEmail} />
+                    <button onClick={this.createUser}>Enviar</button>
                 </MotherDiv>
             </div>
         );
