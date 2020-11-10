b3d4b015ce238026515bd3f49ab4647433ff3d7c
Nicole <nzolnier@gmail.com>
2020-11-10 17:01:24 -0300

    funcao clear

--- semana9/astromatch/src/App.js
+++ semana9/astromatch/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import ClearAll from './components/ClearAll';
 import Home from './components/Home/Home';
 import Match from './components/Matches/Match';
 import NavBar from './components/NavBar';
@@ -6,6 +7,7 @@ import NavBar from './components/NavBar';
 function App() {
   return (
     <div className="App">
+      <ClearAll />
       <Match />
       <p>App</p>
       <Home />
---
+++ semana9/astromatch/src/components/ClearAll.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+const baseUrl = "https://us-central1-missao-newton.cloudfunctions.net/astroMatch/nicole"
+function ClearAll() {
+    const clearAll = () => {
+        const beSure = window.confirm("Tem certeza que quer apagar todos os matches e perfis vistos?")
+
+        if(beSure) {
+            axios.put(`${baseUrl}/clear`).then((res) => {
+                console.log(res)
+            }).catch((err) => {
+                console.log(err.message)
+            })
+        }
+    }
+    
+    return (
+      <div>
+          oii
+        <button onClick={clearAll}>limpar tudo</button>
+      </div>
+    );
+  }
+  
+export default ClearAll;
