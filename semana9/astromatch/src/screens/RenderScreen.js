7d7eeffa0b41438412fb937e6d475d45537f8412
Nicole <nzolnier@gmail.com>
2020-11-13 16:21:56 -0300

    mais um comentario, agora chega

--- semana9/astromatch/src/screens/RenderScreen.js
+++ semana9/astromatch/src/screens/RenderScreen.js
@@ -4,17 +4,20 @@ import { useState } from 'react'
 import Box from '@material-ui/core/Box'
 
 function RenderScreen() {
-
+  // state pra saber a pagina
   const [currentPage, setCurrentPage] = useState("home")
 
+  // setar o current como home
   const goToHome = () => {
     setCurrentPage("home")
   }
 
+  // setar o current como matches
   const goToMatches = () => {
     setCurrentPage("matches")
   }
 
+  // switch pra mudar de pagina, odefault sendo o home
   let renderedPage
   switch (currentPage) {
     case "home":
