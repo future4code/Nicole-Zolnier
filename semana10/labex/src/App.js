a9ab0f88101faa441137db7760cd13cfc6878de3
Nicole <nzolnier@gmail.com>
2020-11-17 13:41:54 -0300

    rotas funcionando sem botoes ainda

--- semana10/labex/src/App.js
+++ semana10/labex/src/App.js
@@ -1,11 +1,8 @@
-import React from 'react'
+import React from "react";
+import Router from "./router/Router";
 
 function App() {
-  return (
-    <div>
-      teste
-    </div>
-  );
+  return <Router />;
 }
 
-export default App;
+export default App
--- semana10/labex/src/router/Router.js
+++ semana10/labex/src/router/Router.js
@@ -1,10 +1,48 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import AboutUsPage from '../pages/AboutUsPage'
+import LandingPage from '../pages/LandingPage'
+import ApplyPage from '../pages/ApplyPage'
+import LoginPage from '../pages/LoginPage'
+import TripsPage from '../pages/TripsPage'
+import ErrorPage from '../pages/ErrorPage'
+import AdminPage from '../pages/AdminPage'
+
+
 
 function Router() {
   return (
-    <div>
-        router
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <LandingPage />
+        </Route>
+
+        <Route exact path="/sobre">
+          <AboutUsPage />
+        </Route>
+        
+        <Route exact path="/inscricao">
+          <ApplyPage />
+        </Route>
+
+        <Route exact path="/login">
+          <LoginPage />
+        </Route>
+
+        <Route exact path="/admin">
+          <AdminPage />
+        </Route>
+        
+        <Route exact path="/viagens">
+          <TripsPage />
+        </Route>
+
+        <Route>
+          <ErrorPage />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
