bfdef6ad8a6bf6ec45a812ae2c443dab9e323f9e
Nicole <nzolnier@gmail.com>
2020-11-18 10:03:28 -0300

    mudando coisas do public e add texto sobre nos

--- semana10/labex/src/pages/Public/AboutUsPage.js
+++ semana10/labex/src/pages/Public/AboutUsPage.js
@@ -5,7 +5,12 @@ function AboutUsPage() {
   return (
     <div>
       <NavBar />
-      <p>AboutUsPage</p>
+      <h1>Sobre a StarkX</h1>
+      <img src="https://picsum.photos/500/400/" />
+      <p>Depois dos acontecimentos do Blip (quando metade da população mundial sumiu), muitas pessoas ficaram com medo do espaço e não queriam mais nem viajar de avião. Nós da StarkX queremos mudar isso e abrir um mundo de oportunidades de diversão para as pessoas. Ao contrário de nossos competidores que querem explorar os outros planetas, nós queremos aprecia-los e mostrar a sua individualidade. Acreditamos que o espaço sideral tenha muitas atividades incríveis e os planetas devem ser preservados em vez de explorados. Por isso, para cada viagem, nós doamos 0.5% das vendas para uma ONG de preservação de planetas.</p>
+      <h1>In memoriam</h1>
+      <p>“Você pode tirar meus trajes, você pode tirar a minha casa, mas há uma coisa que você nunca pode tirar de mim: eu sou o Homem de Ferro!” - Tony Stark</p>
+      <img src="https://picsum.photos/500/400/" />
     </div>
   );
 }
