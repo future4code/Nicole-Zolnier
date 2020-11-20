3f03469a3a7152f3f3fb3101e0a3e4389afd45c1
Nicole <nzolnier@gmail.com>
2020-11-19 22:26:01 -0300

    estilizando inputs, deu desgraça na funcao e no regex

--- semana10/labex/src/pages/Public/ApplyPage.js
+++ semana10/labex/src/pages/Public/ApplyPage.js
@@ -1,18 +1,52 @@
 import axios from 'axios'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import NavBar from '../../components/NavBar'
-import {baseUrl} from '../../constants/urls'
+import { baseUrl } from '../../constants/urls'
 import { useForm } from '../../hooks/useForm'
-import {useTripsList} from '../../hooks/useTripsList'
-import Countries from '../../components/Countries'
+import { useTripsList } from '../../hooks/useTripsList'
 import Footer from '../../components/Footer'
+import { TextField, InputLabel, MenuItem, FormControl, Select, Button, createMuiTheme, MuiThemeProvider } from '@material-ui/core'
+import styled from 'styled-components'
+
+const FormContainer = styled.form`
+	padding: 1em;
+	margin: auto;
+	margin-bottom: 3em;
+	display: flex;
+	flex-direction: column;
+	gap: 1em;
+	width: 30vw;
+`
+const myTheme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#FFFFFF"
+    },
+    secondary: {
+      main: "#67C7EB"
+    }
+  },
+})
+
 
 function ApplyPage() {
   const [trips, loaded] = useTripsList()
-  const { form, onChange, reset } = useForm({ name: "", age: "", reason:"", profession:"", country:"", tripId: null })
+  const { form, onChange, reset } = useForm({ name: "", age: "", reason: "", profession: "", country: "", tripId: "" })
+  const [countries, setCountries] = useState()
+
+  useEffect(() => {
+    getCountries()
+  }, [])
+
+  const getCountries = () => {
+    axios.get("https://restcountries.eu/rest/v2/all").then((res) => {
+      setCountries(res.data)
+    })
+  }
+
 
   const applyToTrip = () => {
-    
+
     const body = {
       name: form.name,
       age: form.age,
@@ -30,34 +64,54 @@ function ApplyPage() {
       console.log(err)
     })
 
-  }  
+  }
   return (
-    
+
     <div>
-        <NavBar />
-        <p>Apply</p>
-        <form onSubmit={applyToTrip}>
-          <input placeholder="nome" value={form.name} onChange={onChange} name="name"
-          type="text"
-          pattern="[a-zA-Z\À-ú ]{3,}"
-          required/>
-          <input placeholder="idade" value={form.age} onChange={onChange}  name="age" type="number"
+      <NavBar />
+      <p>Apply</p>
+      <FormContainer onSubmit={applyToTrip}>
+
+        <TextField label="Nome" value={form.name} onChange={onChange} name="name"
+          inputProps={{ pattern: "[a-zA-Z\À-ú ]{3,}" }}
+          required variant="outlined" />
+        <TextField variant="outlined" label="Idade" value={form.age} onChange={onChange} name="age" type="number"
           min="18"
-          required/>
-          <input pattern="^.{30,}" required placeholder="por que" name="reason" value={form.reason} onChange={onChange}/>
-          <input required  pattern={"[a-zA-Z\À-ú ]{10,}"} placeholder="profissao" name="profession" value={form.profession} onChange={onChange} />
-          <Countries country={form.country} handle={onChange}/>
+          required />
+        <TextField label="Por que você seria um bom candidato?" name="reason" value={form.reason} onChange={onChange}
+          inputProps={{ pattern: "^.{30,}" }}
+          required variant="outlined" />
+        <TextField variant="outlined" required inputProps={{ pattern: "[a-zA-Z\À-ú ]{10,}" }} label="Profissao" name="profession" value={form.profession} onChange={onChange} />
 
-          <select name="tripId" value={form.tripId} onChange={onChange}>
-          <option value="">viagens</option>
-          {trips.map((item) => {
-            return <option value={item.id}>{item.name}</option>
-          })}
-          </select>
 
-        <button>Aplicar</button>
-        </form>
-        <Footer />
+        <FormControl variant="outlined">
+          <InputLabel>País</InputLabel>
+          <Select required label="País" value={form.country} name="country" onChange={onChange} >
+            <MenuItem value="">País</MenuItem>
+            {countries ? countries.map((item) => {
+              return <MenuItem key={item.name}
+                value={item.name}>{item.name}</MenuItem>
+            }) : <MenuItem>Carregando</MenuItem>}
+          </Select>
+        </FormControl>
+
+        <FormControl variant="outlined">
+          <InputLabel>Viagem</InputLabel>
+          <Select label="Viagem" name="tripId" value={form.tripId} onChange={onChange}>
+            <MenuItem value="">Viagem</MenuItem>
+            {trips.map((item) => {
+              return <MenuItem value={item.id}>{item.name}</MenuItem>
+            })}
+          </Select>
+        </FormControl>
+
+        <MuiThemeProvider theme={myTheme}>
+          <Button color="secondary" variant="contained">Aplicar</Button>
+        </MuiThemeProvider>
+
+
+      </FormContainer>
+      <Footer />
     </div>
   );
 }
