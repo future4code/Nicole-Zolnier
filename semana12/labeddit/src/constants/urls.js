638610a7be802a1a682c19672d1f861a66eb6808
Nicole <nzolnier@gmail.com>
2020-11-30 16:53:47 -0300

    funcao login

---
+++ semana12/labeddit/src/constants/urls.js
@@ -0,0 +1,1 @@
+export const baseUrl = "https://us-central1-labenu-apis.cloudfunctions.net/labEddit"
--- semana12/labeddit/src/screens/LoginPage.js
+++ semana12/labeddit/src/screens/LoginPage.js
@@ -1,9 +1,39 @@
 import React from 'react'
+import { useForm } from '../hooks/useForm'
+import axios from 'axios'
+import {baseUrl} from '../constants/urls'
+import { useHistory } from 'react-router-dom'
+
 
 function LoginPage() {
+  const { form, onChange, reset } = useForm({ email: "", password: "" })
+  const history = useHistory()
+
+  const login = (e) => {
+    e.preventDefault()
+
+    const body = {
+      email: form.email,
+      password: form.password
+    };
+
+    axios.post(`${baseUrl}/login`, body).then((res) => {
+      reset()
+      localStorage.setItem("token", res.data.token)
+      history.push("/feed")
+    }).catch((err) => {
+      window.alert("Email ou senha incorretos!")
+      console.log(err)
+    })
+  }
+
   return (
     <div>
-      LoginPage
+      <form onSubmit={login}>
+        <input type={"email"} onChange={onChange} value={form.email} name="email" />
+        <input type={"password"} value={form.password} onChange={onChange} name="password" />
+        <button>LOGAR</button>
+      </form>
     </div>
   );
 }
