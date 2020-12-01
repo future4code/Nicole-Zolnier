1d3be4b56a0be0dcc52bdb7cedfb1ccd3ab2c954
Nicole <nzolnier@gmail.com>
2020-12-01 17:04:30 -0300

    funcao votar

--- semana12/labeddit/src/components/PostCard.js
+++ semana12/labeddit/src/components/PostCard.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import { useHistory } from 'react-router-dom';
 import { goToPostDetails } from '../router/coordinator';
+import { vote } from '../services/feed'
 
 function PostCard(props) {
     const history = useHistory()
 
-    return (<div onClick={() => goToPostDetails(history, props.id)}>
+    return (<div >
         <h3>{props.username}</h3>
-        <h1>{props.title}</h1>
+        <h1 onClick={() => goToPostDetails(history, props.id)}>{props.title} {props.votesCount}</h1>
         <p>{props.text}</p>
+        <button onClick={() => vote(1, props.id)}>gostei</button>
+        <button onClick={() => vote(-1, props.id)}>nao gostei</button>
     </div>
 
     )
--- semana12/labeddit/src/services/feed.js
+++ semana12/labeddit/src/services/feed.js
@@ -7,9 +7,16 @@ const axiosConfig = {
     }
 }
 
+const getPosts = () => {
+    axios.get(`${baseUrl}/posts`, axiosConfig).then(response => {
+    }).catch(err => { 
+    })   
+}
+
 export const createPost = (body) => {
     axios.post(`${baseUrl}/posts`, body, axiosConfig).then((res) => {
         window.alert("deu boa")
+        getPosts()
     }).catch((err) => {
       window.alert("Ih deu erro!")
       console.log(err)
@@ -19,8 +26,24 @@ export const createPost = (body) => {
 export const createComment = (body, postId) => {
     axios.post(`${baseUrl}/posts/${postId}/comment`, body, axiosConfig).then((res) => {
         window.alert("deu boa")
+        getPosts()
     }).catch((err) => {
       window.alert("Ih deu erro!")
       console.log(err)
     })
+}
+
+export const vote = (decision, postId) => {
+    const body = {
+        direction: decision,
+    }
+    
+    axios.put(`${baseUrl}/posts/${postId}/vote`, body, axiosConfig).then((res) => {
+        window.alert("deu boa")
+        getPosts()
+    }).catch((err) => {
+      window.alert("Ih deu erro!")
+      console.log(err)
+    })
+
 }
