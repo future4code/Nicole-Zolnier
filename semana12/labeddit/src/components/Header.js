a276e54d387cbf8fe7b0acff61a3cf1224868e75
Nicole <nzolnier@gmail.com>
2020-12-01 11:29:12 -0300

    header funcionando

--- semana12/labeddit/src/components/Header.js
+++ semana12/labeddit/src/components/Header.js
@@ -1,11 +1,41 @@
 import React from 'react'
+import { goToLogin, goToSignUp } from '../router/coordinator'
+import styled from 'styled-components'
+import { useHistory } from 'react-router-dom'
+
+const HeaderContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+  background-color: green;
+  height: 8vh;
+  align-items: center;
+`
+
 
 function Header() {
-  return (
-    <div>
-      Header
-    </div>
-  );
+  const history = useHistory()
+  const token = localStorage.getItem("token")
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    goToLogin(history)
+  }
+
+
+  if (token) {
+    return (<HeaderContainer>
+      <h1>Logo</h1>
+      <h2 onClick={logout}>Logout</h2>
+      <h1>usuario</h1>
+    </HeaderContainer>
+    )
+  } else {
+    return (<HeaderContainer>
+      <h1>Logo</h1>
+      <button onClick={() => goToLogin(history)}>Log in</button>
+      <button onClick={() => goToSignUp(history)}>Cadastrar</button>
+    </HeaderContainer>)
+  }
 }
 
 export default Header;
--- semana12/labeddit/src/router/Router.js
+++ semana12/labeddit/src/router/Router.js
@@ -5,24 +5,30 @@ import SignUpPage from '../screens/SignUpPage'
 import FeedPage from '../screens/FeedPage'
 import PostDetailsPage from '../screens/PostDetailsPage'
 import ErrorPage from '../screens/ErrorPage'
+import Header from '../components/Header';
 
 function Router() {
     return (
         <BrowserRouter>
             <Switch>
                 <Route exact path="/">
+                    <Header />
                     <LoginPage />
                 </Route>
                 <Route exact path="/cadastrar">
+                    <Header />
                     <SignUpPage />
                 </Route>
                 <Route exact path="/feed">
+                    <Header />
                     <FeedPage />
                 </Route>
                 <Route exact path="/feed/post/:id">
+                    <Header />
                     <PostDetailsPage />
                 </Route>
                 <Route>
+                    <Header />
                     <ErrorPage />
                 </Route>
             </Switch>
