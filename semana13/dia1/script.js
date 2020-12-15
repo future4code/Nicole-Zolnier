e2f99b8a30eee565748bc2e341aae44ec275f76e
Nicole <nzolnier@gmail.com>
2020-12-15 11:09:09 -0300

    exercicio 2

--- semana13/dia1/script.js
+++ semana13/dia1/script.js
@@ -1,8 +1,26 @@
-const answer = prompt("Escreva algo")
+// ex 1 - funcao que reverte um texto
+
+/* const answer = prompt("Escreva algo").toLowerCase()
 
 const reverseText = (string) => {
     return string.split("").reverse().join("")
 }
 
 console.log(answer)
-console.log(reverseText(answer))
+console.log(reverseText(answer)) */
+
+// ex 2 - palindromo
+
+const answer = prompt("Escreva uma palavra massa").toLowerCase()
+
+const checkIfPalindrome = (string) => {
+    const reverseString = string.split("").reverse().join("")
+
+    if(reverseString === string){
+        return `${string} é um palindromo`
+    } else {
+        return `${string} não é um palindromo`
+    }
+}
+
+console.log(checkIfPalindrome(answer))
