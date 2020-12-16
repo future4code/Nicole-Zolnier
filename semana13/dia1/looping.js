365f5d928475b61b6eea6dd433abbcbc2f423102
Nicole <nzolnier@gmail.com>
2020-12-16 14:51:57 -0300

    ex 1 - lacos

---
+++ semana13/dia1/looping.js
@@ -0,0 +1,14 @@
+// ---- ex 1 - maior numero
+/* const numbersArray = [1, 3, 9, 5 , 2, 7]
+const highestNumber = (array) => {
+    let highest = 0
+    for (let number of array) {
+        if (number > highest) {
+            highest = number
+        }
+    }
+    return `O maior numero é ${highest}`
+}
+
+console.log(highestNumber(numbersArray)) */
+
