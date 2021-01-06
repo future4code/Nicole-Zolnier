b17416a77b23731f22f0f936b8d52de0678b9195
Nicole <nzolnier@gmail.com>
2021-01-06 13:36:34 -0300

    endpoint 1

--- semana 15/aula 42/src/index.ts
+++ semana 15/aula 42/src/index.ts
@@ -6,6 +6,14 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
+app.get('/countries/all', (req: Request, res: Response) => {
+    const result = countries.map((country) => ({
+        id: country.id,
+        name: country.name
+    }))
+    res.status(200).send({result})
+})
+
 app.listen(3003, () => {
     console.log("Server is running in http://localhost:3003");
    });
