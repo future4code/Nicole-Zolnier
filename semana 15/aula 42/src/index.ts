2ba9f5fc33d7537f3bfac62e399b06ba73ae0b70
Nicole <nzolnier@gmail.com>
2021-01-06 14:04:29 -0300

    endpoint 2

--- semana 15/aula 42/src/index.ts
+++ semana 15/aula 42/src/index.ts
@@ -1,18 +1,30 @@
 import express, { Request, Response } from 'express'
 import cors from 'cors'
-import {countries} from './countries'
+import {countries, country} from './countries'
 
 const app = express()
 app.use(express.json())
 app.use(cors())
 
-app.get('/countries/all', (req: Request, res: Response) => {
+app.get("/countries/all", (req: Request, res: Response) => {
     const result = countries.map((country) => ({
         id: country.id,
         name: country.name
     }))
     res.status(200).send({result})
 })
+
+app.get("/countries/:id", (req: Request, res: Response) => {
+    const result: country | undefined = countries.find(
+       country => country.id === Number(req.params.id)
+    )
+ 
+    if (result) {
+        res.status(200).send(result)
+     } else {
+        res.status(404).send("País não encontrado")
+     }
+ })
 
 app.listen(3003, () => {
     console.log("Server is running in http://localhost:3003");
