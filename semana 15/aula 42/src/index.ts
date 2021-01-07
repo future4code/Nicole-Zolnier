ca338f9b524d571da1c8354d95c07027eaa34c78
Nicole <nzolnier@gmail.com>
2021-01-06 21:59:27 -0300

    endpoint 4 dalee

--- semana 15/aula 42/src/index.ts
+++ semana 15/aula 42/src/index.ts
@@ -14,11 +14,41 @@ app.get("/countries/all", (req: Request, res: Response) => {
     }))
     res.status(200).send({ result })
 })
+
+// endpoint 4 - edit country
+app.put("/countries/edit/:id", (req: Request, res: Response) => {
+    const result = countries.findIndex(
+      (country) => country.id === Number(req.params.id)
+    );
+  
+    countries[result].name = req.body.name;
+    countries[result].capital = req.body.capital;
+  
+    if (result) {
+      res.status(200).send("País alterado com sucesso!");
+    } else {
+      res.status(404).send("País não encontrado");
+    }
+  });
+
+// endpoint 2 - get country by id
+app.get("/countries/:id", (req: Request, res: Response) => {
+    const result: country | undefined = countries.find(
+        country => country.id === Number(req.params.id)
+    )
+
+    if (result) {
+        res.status(200).send(result)
+    } else {
+        res.status(404).send("País não encontrado")
+    }
+})
+
 // endpoint 3 - search by name, capital, continent
 app.get("/countries/search", (req: Request, res: Response) => {
     let result: country[] = countries
 
-    if(!req.query.name && !req.query.capital && !req.query.continent){
+    if (!req.query.name && !req.query.capital && !req.query.continent) {
         res.status(404).send("Coloque um parametro (name, capital ou continent)")
     }
     if (req.query.name) {
@@ -37,7 +67,7 @@ app.get("/countries/search", (req: Request, res: Response) => {
         )
     }
 
-    if(result.length !== 0){
+    if (result.length !== 0) {
         res.status(200).send(result)
     } else {
         res.status(404).send("Pais não encontrado")
@@ -45,63 +75,34 @@ app.get("/countries/search", (req: Request, res: Response) => {
 
 })
 
-// endpoint 4 - edit country
-app.put("countries/edit/:id", (req: Request, res: Response) => {
-    const result: country | undefined = countries.find(
-        country => country.id === Number(req.params.id)
-    )
-    if(result){
-        result.name = req.body.name
-        result.capital = req.body.capital
-        res.status(200).send(result);
-    } else {
-        res.status(404).send("Deu erro")
-    }
-    
-    
-})
-
-// endpoint 2 - get country by id
-app.get("/countries/:id", (req: Request, res: Response) => {
-    const result: country | undefined = countries.find(
-        country => country.id === Number(req.params.id)
-    )
-
-    if (result) {
-        res.status(200).send(result)
-    } else {
-        res.status(404).send("País não encontrado")
-    }
-})
-
 
 
 // endpoint 5 - delete country
-app.delete("/countries/:id", (req:Request, res:Response)=>{
+app.delete("/countries/:id", (req: Request, res: Response) => {
     let errorCode: number = 400
     try {
- 
-       if(!req.headers.authorization){
-          errorCode = 401
-          throw new Error()
-       }
- 
-       const countryIndex: number = countries.findIndex(
-          (country) => country.id === Number(req.params.id)
-       )
- 
-          if(countryIndex === -1){
-             errorCode = 404
-             throw new Error()
-          }
- 
-       countries.splice(countryIndex, 1)
-       res.status(200).end()
+
+        if (!req.headers.authorization) {
+            errorCode = 401
+            throw new Error()
+        }
+
+        const countryIndex: number = countries.findIndex(
+            (country) => country.id === Number(req.params.id)
+        )
+
+        if (countryIndex === -1) {
+            errorCode = 404
+            throw new Error()
+        }
+
+        countries.splice(countryIndex, 1)
+        res.status(200).end()
     } catch (error) {
-       console.log(error)
-       res.status(errorCode).end()
+        console.log(error)
+        res.status(errorCode).end()
     }
- })
+})
 
 //  endpoint 6 - create country
 app.post("countries/create", (req: Request, res: Response) => {
