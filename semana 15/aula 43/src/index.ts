3bb3a2abb5bd041a20d012cfb030927f5c3b0b72
Nicole <nzolnier@gmail.com>
2021-01-07 13:53:00 -0300

    ex 1

--- semana 15/aula 43/src/index.ts
+++ semana 15/aula 43/src/index.ts
@@ -1,6 +1,7 @@
 //importando express com Request e Response e cors
 import express, {Request, Response} from 'express';
 import cors from 'cors';
+import { users, user } from './users'
 
 //extra: importando configuração de rede do node
 import { AddressInfo } from "net";
@@ -11,61 +12,21 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-type user = {
-    id: number,
-    name: string,
-    email: string,
-    type: string,
-    age: number
-}
+// ex 1 - endpoint que retorna todos os endpoints
+// a) metodo GET
+// b) a entidade é o users
 
+app.get("/user/all", (req: Request, res: Response) => {
+    let errorCode: number = 400;
+    try {
+        const result = users
+        res.status(200).send(result);
 
-let users: user[] = [
-    {
-        id: 1,
-        name: "Alice",
-        email: "alice@email.com",
-        type: "ADMIN",
-        age: 12
-    },
-    {
-        id: 2,
-        name: "Bob",
-        email: "bob@email.com",
-        type: "NORMAL",
-        age: 36
-    },
-    {
-        id: 3,
-        name: "Coragem",
-        email: "coragem@email.com",
-        type: "NORMAL",
-        age: 21
-    },
-    {
-        id: 4,
-        name: "Dory",
-        email: "dory@email.com",
-        type: "NORMAL",
-        age: 17
-    },
-    {
-        id: 5,
-        name: "Elsa",
-        email: "elsa@email.com",
-        type: "ADMIN",
-        age: 17
-    },
-    {
-        id: 6,
-        name: "Fred",
-        email: "fred@email.com",
-        type: "ADMIN",
-        age: 60
+    } catch (error) {
+        res.status(errorCode).send(error.message);
     }
-]
 
-
+});
 
 
 const server = app.listen(process.env.PORT || 3003, () => {
