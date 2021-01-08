83443180af01538f3ca996b002f4d8bf7032459e
Nicole <nzolnier@gmail.com>
2021-01-08 15:10:24 -0300

    create account sem verificação ainda

--- semana 15/sistema-bancario/src/index.ts
+++ semana 15/sistema-bancario/src/index.ts
@@ -8,6 +8,31 @@ const app: Express = express();
 app.use(express.json());
 app.use(cors());
 
+app.post("/account/create", (req: Request, res: Response) => {
+    let errorCode: number = 400;
+    try {
+        const newUser: userAccount = {
+            name: req.body.name,
+            cpf: req.body.cpf,
+            birthDate: req.body.birthDate,
+            balance: 0,
+            statement: []
+        }
+
+        if (!newUser.name || !newUser.cpf || !newUser.birthDate) {
+            errorCode = 422;
+            throw new Error("Algum campo está inválido. Preencha corretamente.");
+        }
+
+        users.push(newUser);
+
+        res.status(200).send({ message: "Usuário inserido com sucesso!" });
+
+    } catch (error) {
+        res.status(errorCode).send({ message: error.message });
+    }
+})
+
 app.get("/account/all", (req: Request, res: Response) => {
     let errorCode: number = 400;
     try {
