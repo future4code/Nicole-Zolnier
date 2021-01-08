a670f7e4e2b8a6324256c67e4ab7e25df00dc9ef
Nicole <nzolnier@gmail.com>
2021-01-08 14:29:59 -0300

    get all accounts

--- semana 15/sistema-bancario/src/index.ts
+++ semana 15/sistema-bancario/src/index.ts
@@ -1,13 +1,25 @@
 import express, {Express, Request, Response} from 'express'
 import cors from 'cors'
-import { AddressInfo } from "net";
+import { AddressInfo } from "net"
+import { users, userAccount } from './users'
 
 const app: Express = express();
 
 app.use(express.json());
 app.use(cors());
 
+app.get("/account/all", (req: Request, res: Response) => {
+    let errorCode: number = 400;
+    try {
+        const result: userAccount[] = users
 
+        res.status(200).send(result);
+
+    } catch (error) {
+        res.status(errorCode).send(error.message);
+    }
+
+});
   
 const server = app.listen(process.env.PORT || 3003, () => {
     if (server) {
