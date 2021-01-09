5f6417948abad7ca1c12bb8fdc4db1e063d740b1
Nicole <nzolnier@gmail.com>
2021-01-09 14:25:53 -0300

    endpoint pagar conta com verificaçoes

--- semana 15/sistema-bancario/src/helpers.ts
+++ semana 15/sistema-bancario/src/helpers.ts
@@ -11,18 +11,18 @@ export const getTimestamp = (date: string): number => {
 // console.log(today)
 export const checkIfAdult = (birthDate: string): boolean => {
 
-    const birthDateTimestamp = getTimestamp(birthDate)
+    const birthDateTimestamp: number = getTimestamp(birthDate)
 
-    const ageInMilli : number = (today - birthDateTimestamp) / 1000
+    const ageInMilli: number = (today - birthDateTimestamp) / 1000
 
-    const age = ageInMilli/(60 * 60 * 24 * 365)
+    const age: number = ageInMilli/(60 * 60 * 24 * 365)
 
     return age > 18
 }
 
 // console.log(checkIfAdult("22/02/2002"))
 
-export const checkCpf = (cpf: number): userAccount | undefined => {
+export const findCpf = (cpf: number): userAccount | undefined => {
     return users.find(account => {
             return account.cpf === cpf
     })
--- semana 15/sistema-bancario/src/index.ts
+++ semana 15/sistema-bancario/src/index.ts
@@ -2,7 +2,7 @@ import express, {Express, Request, Response} from 'express'
 import cors from 'cors'
 import { AddressInfo } from "net"
 import { users, userAccount, transaction } from './users'
-import { checkIfAdult, checkCpf } from './helpers'
+import { today, checkIfAdult, findCpf, getTimestamp } from './helpers'
 
 const app: Express = express();
 
@@ -12,6 +12,7 @@ app.use(cors());
 app.post("/account/create", (req: Request, res: Response) => {
     let errorCode: number = 400
     try {
+        
         const newUser: userAccount = {
             name: req.body.name,
             cpf: req.body.cpf,
@@ -25,14 +26,19 @@ app.post("/account/create", (req: Request, res: Response) => {
             throw new Error("Algum campo está inválido. Preencha corretamente.")
         }
 
-        const canOpenAccount: boolean = checkIfAdult(req.body.birthDate) 
-
+        if(!getTimestamp(req.body.birthDate)){
+            errorCode = 422
+            throw new Error("Formato errado. Por favor siga o formato DD/MM/YYYY")
+        } 
+        
+        const canOpenAccount: boolean = checkIfAdult(req.body.birthDate)
+    
         if(!canOpenAccount) {
             errorCode = 401
             throw new Error("Você precisa ter 18 ou mais de 18 para criar uma conta")
         }
 
-        const existingAccount : userAccount | undefined = checkCpf(newUser.cpf)
+        const existingAccount : userAccount | undefined = findCpf(newUser.cpf)
 
         if(existingAccount){
             errorCode = 409
@@ -48,6 +54,52 @@ app.post("/account/create", (req: Request, res: Response) => {
     }
 })
 
+app.post("/account/pay", (req: Request, res: Response) => {
+    let errorCode: number = 400
+    try {
+        if (!req.body.cpf || !req.body.amount) {
+            errorCode = 422
+            throw new Error("Algum campo está inválido, por favor informe CPF e valor.")
+        }
+    
+        const existingAccount: userAccount | undefined = findCpf(req.body.cpf)
+
+        if(!existingAccount){
+            errorCode = 404
+            throw new Error("Conta não encontrada")
+        } else if(existingAccount.balance < Number(req.body.amount)) {
+            throw new Error("Saldo insuficiente") 
+        } else {
+            let date: any = req.body.date
+
+            if(!date){
+                date = today
+            } else if(!getTimestamp(date)){
+                throw new Error("Formato errado. Por favor siga o formato DD/MM/YYYY")
+            } else {
+                date = getTimestamp(date)
+                if(date < today){
+                    throw new Error("Essa data já passou.")
+                }
+            }
+
+            const newPayment = {
+                amount: -req.body.amount,
+                date: date,
+                description: req.body.description || ""
+            }
+
+            existingAccount.balance -= req.body.amount
+            existingAccount.statement.push(newPayment)
+
+            res.status(202).send("Pagamento agendado com sucesso!")
+        }
+    
+    } catch (error) {
+        res.status(errorCode).send({ message: error.message })
+    }
+})
+
 app.put("/account/deposit", (req: Request, res: Response) => {
     let errorCode: number = 400
     try {
@@ -56,11 +108,11 @@ app.put("/account/deposit", (req: Request, res: Response) => {
             throw new Error("Algum campo está inválido. Preencha corretamente.")
         }
 
-        const existingAccount: userAccount | undefined = checkCpf(req.body.cpf)
+        const existingAccount: userAccount | undefined = findCpf(req.body.cpf)
 
         if(!existingAccount){
             errorCode = 404
-            throw new Error()
+            throw new Error("Conta não encontrada")
         } else {
             const newTransaction: transaction = { 
                 amount: Number(req.body.amount),
@@ -86,7 +138,7 @@ app.get("/account/search", (req: Request, res: Response) => {
             throw new Error("Insira um CPF válido")
         }
 
-        const checkingCpf: userAccount | undefined = checkCpf(cpf)
+        const checkingCpf: userAccount | undefined = findCpf(cpf)
 
         if(!checkingCpf) {
             errorCode = 404
