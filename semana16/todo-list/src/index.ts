ea8e8cf666125fa2c360253ec91ec99700e21216
Nicole <nzolnier@gmail.com>
2021-01-14 21:37:33 -0300

    criando arquivos

---
+++ semana16/todo-list/src/index.ts
@@ -0,0 +1,33 @@
+import express, { Express } from "express";
+import knex from "knex";
+import cors from "cors";
+import dotenv from "dotenv";
+import { AddressInfo } from "net";
+
+dotenv.config();
+
+export const connection = knex({
+   client: "mysql",
+   connection: {
+      host: process.env.DB_HOST,
+      port: 3306,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME
+   }
+})
+
+const app: Express = express();
+app.use(express.json());
+app.use(cors())
+
+// endpoints aqui
+
+const server = app.listen(process.env.PORT || 3003, () => {
+   if (server) {
+      const address = server.address() as AddressInfo;
+      console.log(`Server is running in http://localhost:${address.port}`);
+   } else {
+      console.error(`Failure upon starting server.`);
+   }
+});
