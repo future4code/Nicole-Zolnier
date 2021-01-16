47470d7a935f45e0b2356375691e5fb8171fc1f3
Nicole <nzolnier@gmail.com>
2021-01-16 11:29:33 -0300

    edit user

--- semana16/todo-list/src/endpoints/editUser.ts
+++ semana16/todo-list/src/endpoints/editUser.ts
@@ -1,10 +1,29 @@
 import express, { Router, Request, Response } from 'express'
 import cors from 'cors'
+import { editUser } from '../data/editUser'
 
 const router: Router = express.Router()
 router.use(express.json())
 router.use(cors())
 
-router.post('/user/edit/:id', (req: Request, res: Response) => {
+router.post('/edit/:id', async (req: Request, res: Response) => {
+    let errorCode = 400
+    try {
+        const id: number = Number(req.params.id)
+        const { name, nickname } = req.body
 
+        if (!name || !nickname) {
+            errorCode = 422
+            throw new Error("Please provide a new name and a new nickname")
+        } else {
+            await editUser(id, name, nickname)
+            res.status(201).send("User edited!")
+        }
+
+        
+    } catch (error) {
+        res.status(errorCode).send(error.message || error.sqlMessage)
+    }
 })
+
+export default router
--- semana16/todo-list/src/index.ts
+++ semana16/todo-list/src/index.ts
@@ -2,12 +2,13 @@ import express, { Express } from "express";
 import { AddressInfo } from "net";
 import createUser from './endpoints/createUser'
 import getUserById from './endpoints/getUserById'
+import editUser from './endpoints/editUser'
 
 const app: Express = express();
 
 app.use('/user',createUser)
 app.use('/user', getUserById)
-// app.use('/user/edit/:id', editUser)
+app.use('/user', editUser)
 // app.use('/task', createTask)
 // app.use('/task/:id', getTaskById)
 
