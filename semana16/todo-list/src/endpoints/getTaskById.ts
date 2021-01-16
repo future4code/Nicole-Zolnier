518f587352d192aca291e4b62802edca89442b08
Nicole <nzolnier@gmail.com>
2021-01-16 12:53:23 -0300

    get task by id sem observacoes

--- semana16/todo-list/src/endpoints/getTaskById.ts
+++ semana16/todo-list/src/endpoints/getTaskById.ts
@@ -1,12 +1,28 @@
 import express, { Router, Request, Response } from 'express'
 import cors from 'cors'
+import { getTaskById } from '../data/getTaskById'
 
 const router: Router = express.Router()
 router.use(express.json())
 router.use(cors())
 
-router.get('/:id', (req: Request, res: Response) => {
-
+router.get('/:id', async (req: Request, res: Response) => {
+    let errorCode = 400
+    try {
+      const id = Number(req.params.id)
+  
+      const result = await getTaskById(id)
+  
+      if (!result) {
+        errorCode = 404
+        throw new Error("Task not found")
+      } else {
+        res.status(200).send(result)
+      }
+  
+    } catch (error) {
+      res.status(errorCode).send(error.message || error.sqlMessage)
+    }
 })
 
 export default router
--- semana16/todo-list/src/index.ts
+++ semana16/todo-list/src/index.ts
@@ -4,7 +4,7 @@ import createUser from './endpoints/createUser'
 import getUserById from './endpoints/getUserById'
 import editUser from './endpoints/editUser'
 import createTask from './endpoints/createTask'
-// import getTaskById from './endpoints/getTaskById'
+import getTaskById from './endpoints/getTaskById'
 
 const app: Express = express();
 
@@ -12,13 +12,13 @@ app.use('/user',createUser)
 app.use('/user', getUserById)
 app.use('/user', editUser)
 app.use('/task', createTask)
-// app.use('/task', getTaskById)
+app.use('/task', getTaskById)
 
 const server = app.listen(process.env.PORT || 3003, () => {
    if (server) {
       const address = server.address() as AddressInfo;
-      console.log(`Server is running in http://localhost:${address.port}`);
+      console.log(`Server is running in http://localhost:${address.port}`)
    } else {
-      console.error(`Failure upon starting server.`);
+      console.error(`Failure upon starting server.`)
    }
-});
+})
