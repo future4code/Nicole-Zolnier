a9a2baf0c3cb6d9a810ee7b80c808649825cdb39
Nicole <nzolnier@gmail.com>
2021-01-18 15:03:43 -0300

    modificacao no status code

--- semana17/aula48/src/endpoints/getUserByName.ts
+++ semana17/aula48/src/endpoints/getUserByName.ts
@@ -3,24 +3,23 @@ import selectByName from '../data/selectByName'
 import { user } from '../types/user'
 
 export const getUserByName = async (req:Request, res:Response) => {
-    let errorCode = 400
     try {
         const name:string = req.params.name as string
 
         if(!name) {
-            errorCode = 406
+            res.statusCode = 406
             throw new Error("Please provide a name.");
         } else {
             const users: user[] = await selectByName(name)
 
             if(!users.length) {
-                errorCode = 404
+                res.statusCode = 404
                 throw new Error("No user found with that name");
             }
 
             res.status(200).send(users)
         }
     } catch (error) {
-        res.status(errorCode).send(error.message || error.sqlMessage)
+        res.send(error.message || error.sqlMessage)
     }
 }
--- semana17/aula48/src/endpoints/getUserByType.ts
+++ semana17/aula48/src/endpoints/getUserByType.ts
@@ -3,24 +3,24 @@ import selectByType from '../data/selectByType'
 import { user } from '../types/user'
 
 export const getUserByType = async (req:Request, res:Response) => {
-    let errorCode = 400
+
     try {
         const type:string = req.params.type as string
 
         if(!type) {
-            errorCode = 406
+            res.statusCode = 406
             throw new Error("Please provide a type.");
         } else {
             const users: user[] = await selectByType(type)
 
             if(!users.length) {
-                errorCode = 404
+                res.statusCode = 404
                 throw new Error("No user found with that type");
             }
 
             res.status(200).send(users)
         }
     } catch (error) {
-        res.status(errorCode).send(error.message || error.sqlMessage)
+        res.send(error.message || error.sqlMessage)
     }
 }
