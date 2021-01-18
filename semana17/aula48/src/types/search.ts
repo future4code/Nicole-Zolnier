145835b9ff92ea9d397bbf5acb0825813095cf47
Nicole <nzolnier@gmail.com>
2021-01-18 16:12:40 -0300

    ex 4

--- semana17/aula48/src/types/search.ts
+++ semana17/aula48/src/types/search.ts
@@ -2,3 +2,11 @@ export type search = {
     name: string,
     type: string
 }
+
+export type bigSearch = {
+    name: string,
+    type: string,
+    orderBy: string,
+    orderType: string,
+    page: number
+}
