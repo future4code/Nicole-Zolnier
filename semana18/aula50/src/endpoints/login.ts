1f12ba015912d49ab4790fbf77154a11d0caac5c
Nicole <nzolnier@gmail.com>
2021-01-25 15:16:30 -0300

    ex 6

--- semana18/aula50/src/endpoints/login.ts
+++ semana18/aula50/src/endpoints/login.ts
@@ -1,15 +1,10 @@
 import { Request, Response } from "express";
-import { insertUser } from "../data/insertUser";
-import { generateId } from "../services/generateId";
+import { selectUserByEmail } from "../data/selectUserByEmail";
 import { generateToken } from "../services/generateToken";
-import { User } from "../types";
 
-
-export const createUser = async (req:Request, res:Response) => {
+export const login = async (req:Request, res:Response) => {
     const { email, password } = req.body
     try {
-        const id: string = generateId()
-
         if(!email || email.indexOf("@") === -1){
             res.statusCode = 422
             throw new Error("Invalid email")
@@ -20,17 +15,25 @@ export const createUser = async (req:Request, res:Response) => {
             throw new Error("Invalid password. Make sure it has more than 6 characters")
         }
 
-
-        const newUser: User = {
-            id: id,
+        const newUser = {
             email: email,
             password: password
         }
 
-        await insertUser(newUser);
+        const user = await selectUserByEmail(newUser.email);
+
+        if(!user){
+            res.statusCode = 404
+            throw new Error("User not found");
+        }
+
+        if (user.password !== newUser.password) {
+            res.statusCode = 401
+            throw new Error("Invalid password");
+        }
 
         const token = generateToken({
-            id,
+            id: user.id,
         });
       
           res.status(200).send({
--- semana18/aula50/src/index.ts
+++ semana18/aula50/src/index.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import { AddressInfo } from "net";
 import { createUser } from "./endpoints/createUser";
+import { login } from "./endpoints/login"
 
 const app = express();
 app.use(express.json());
 
 app.post('/signup', createUser)
+app.post('/login', login)
 
 const server = app.listen(process.env.PORT || 3000, () => {
   if (server) {
