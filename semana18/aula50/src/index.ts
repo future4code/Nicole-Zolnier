d8b3bb9efe00bc1c9dfe4468fa4ae87b8102fd27
Nicole <nzolnier@gmail.com>
2021-01-25 12:50:36 -0300

    criando arquivos

---
+++ semana18/aula50/src/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import { AddressInfo } from "net";
+
+const app = express();
+app.use(express.json());
+
+const server = app.listen(process.env.PORT || 3000, () => {
+  if (server) {
+    const address = server.address() as AddressInfo;
+    console.log(`Server is running in http://localhost:${address.port}`);
+  } else {
+    console.error(`Failure upon starting server.`);
+  }
+});
