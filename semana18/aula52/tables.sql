6a45700c563224da1f9db2ccc07b9a4862d2ec50
Nicole <nzolnier@gmail.com>
2021-01-27 14:58:41 -0300

    ex 2

--- semana18/aula52/tables.sql
+++ semana18/aula52/tables.sql
@@ -10,3 +10,16 @@ SELECT * from Users_Aula50;
 
 ALTER TABLE Users_Aula50
 ADD role ENUM("NORMAL", "ADMIN") DEFAULT("NORMAL");
+
+CREATE TABLE Users_Address (
+	id VARCHAR(255) UNIQUE PRIMARY KEY,
+    street VARCHAR(255) NOT NULL,
+    number VARCHAR(255) NOT NULL,
+    complement VARCHAR(255),
+    neighborhood VARCHAR(255) NOT NULL,
+    city VARCHAR(255) NOT NULL,
+    state VARCHAR(255) NOT NULL,
+    user_id VARCHAR(255) UNIQUE NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES Users_Aula50(id)
+);
+
