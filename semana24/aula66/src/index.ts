0695dce3fd99ab04eb616c2454e7221f349af9c2
Nicole <nzolnier@gmail.com>
2021-03-17 11:01:16 -0300

    desafios :)

--- semana24/aula66/src/index.ts
+++ semana24/aula66/src/index.ts
@@ -50,3 +50,49 @@ const printArray = (array: number[], index: number = 0) => {
 }
 
 console.log(printArray([1,2,3,4,5]))
+
+// ----------- ex 5
+const getDigits = (number: number, digits: number = 1): number => {
+    if (number < 10) {
+      return digits
+    }
+    return getDigits(number / 10, digits + 1)
+}
+
+console.log(getDigits(0))
+console.log(getDigits(10))
+console.log(getDigits(12020))
+  
+// ----------- ex 6
+const getLargestNumber = (array: number[], index: number = 0, largest: number = 0): number => {
+    if (index < array.length) {
+        if (array[index] > largest) {
+            largest = array[index]
+        }
+
+        return getLargestNumber(array, index + 1, largest)
+    }
+
+    return largest
+}
+
+console.log(getLargestNumber([34, 21, 42, 13, 72, 64, 100]))
+
+// ----------- ex 7
+const findCapitalLetter = (string: string, char: string = ""): string => {
+    if (char && char.toUpperCase() === char && char !== " ") {
+        return char;
+      }
+      return findCapitalLetter(string.substring(1), string[0]);
+}
+
+console.log(findCapitalLetter("potaTo"))
+
+// ----------- ex 8
+
+// O(b) porque depende de b
+
+// ----------- ex 9
+
+// O(2^n) porque tem nó
+
