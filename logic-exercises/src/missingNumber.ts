a1f55b8930d6c4b7d2313aeed6e8e6f9ab3d903a
Nicole <nzolnier@gmail.com>
2021-03-22 11:38:31 -0300

    exercicio do dia

---
+++ logic-exercises/src/missingNumber.ts
@@ -0,0 +1,16 @@
+const findMissingNumber = (arr: number[], n: number = arr.length) => {
+    let total = ((n + 1) * (n + 2)) / 2
+        for (let i = 0; i < n; i++){
+            total -= arr[i]
+            return total
+        }
+            
+}
+
+console.log(findMissingNumber([1,2,3,5,6,7]))
+
+// original equation would be (n(n+1))/2
+// but since there is one missing we add this one to both items
+// (n+1(n+1+1))/2
+// ((n+1)(n+2))/2
+// yay :)
