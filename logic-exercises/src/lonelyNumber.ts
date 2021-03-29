7b26c27e1e89db3fc8d8b26318992c0b8d4b60f6
Nicole <nzolnier@gmail.com>
2021-03-29 14:52:11 -0300

    update desafio

--- logic-exercises/src/lonelyNumber.ts
+++ logic-exercises/src/lonelyNumber.ts
@@ -1,7 +1,4 @@
 export const lonelyNumber = (arr: number[]) => {
-    let unique = arr.filter(function (value: any) {
-        return arr.indexOf(value) === arr.lastIndexOf(value)
-    })
-
-    return unique[0]
+    let unique = arr.reduce((a, b) => a ^ b, 0)
+    return unique
 }
